b1ccfd7b955c8bf3da65089acda7187921fda013
Bola <bolanaguib@gmail.com>
2020-08-20 11:43:30 +0200

    [ added ] navbar styling

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	purge: [
+		// purge: {
+		// 	enabled: true,
+		// 	content: ["./**/*.php"],
+		// },
+	],
+	theme: {
+		extend: {
+			colors: {
+				main: "#303030",
+				secondary: "#fed700",
+			},
+			fontFamily: {
+				body: ["Lobster Two", "cursive"],
+				display: ["Lobster Two", "cursive"],
+				number: ["sans-serif"],
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
