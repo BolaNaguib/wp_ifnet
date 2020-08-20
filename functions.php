cc8e9fe841efd1ac5c824a343c1a132404199421
Bola <bolanaguib@gmail.com>
2020-08-20 14:46:08 +0200

    [ Added ] Gallery section

--- functions.php
+++ functions.php
@@ -1,13 +1,15 @@
-<?php 
+<?php
 
 function add_enqueue_styles()
 {
     wp_enqueue_style('google_font',  'https://fonts.googleapis.com/css2?family=Lobster+Two:wght@400;700&display=swap');
     // wp_enqueue_script('uikit_js',  get_template_directory_uri() . '/assets/js/custom.min.js', array(), false, true);
+    // wp_enqueue_script('vendorJS',  get_template_directory_uri() . '/assets/js/vendor.min.js', array(), false, true);
+    wp_enqueue_script('vendorJS',  get_template_directory_uri() . '/assets/js/vendor/flickity.pkgd.min.js', array(), false, true);
+
     wp_enqueue_style('theme_style',  get_template_directory_uri() . '/style.min.css', 1);
+    wp_enqueue_style('flickityCSS',  get_template_directory_uri() . '/assets/css/flickity.css', 2);
 }
 
 
 add_action('wp_enqueue_scripts', 'add_enqueue_styles', 10);
-
-?>
--- index.php
+++ index.php
@@ -115,6 +115,77 @@
         </div>
     </section>
     <!-- END gbSection -->
+
+    <!-- START testimonials and partners  -->
+    <section class="py-12">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-2 gap-6">
+                <div>
+                    <h4>
+
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END testimonials and partners  -->
+
+
+
+    <!-- START NEWS -->
+    <section class="py-12 bg-gray-100">
+        <div class="container mx-auto px-4">
+            <div class="text-center py-8 mb-8">
+                <h2 class="text-4xl">News, Updates & Tricks</h2>
+            </div>
+            <div class="grid lg:grid-cols-3 gap-6">
+                <?php for ($i = 0; $i < 3; $i++) : ?>
+                    <div class="max-w-2xl mx-auto bg-white overflow-hidden shadow-md rounded-lg">
+                        <img class="w-full h-64 object-cover" src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Article">
+
+                        <div class="p-6">
+                            <div>
+                                <a href="#" class="block text-gray-800 font-semibold text-2xl mt-2 hover:text-gray-600 hover:underline">Secure strong revenue growth</a>
+                                <p class="text-sm text-gray-600 mt-2">Sed odio orci, fringilla nec dolor et, euismod auctor mauris. Curabitur semper dui diam, nec accumsan mauris consequat sed. Interdum et malesuada fames ac ante…</p>
+                            </div>
+
+                            <div class="mt-4">
+                                <div class="flex items-center justify-between">
+                                    <a href="#" class="px-2 py-1 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700">READ MORE</a>
+
+                                    <span class="mx-1 text-gray-600 text-xs">21 SEP 2015</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php endfor; ?>
+
+            </div>
+        </div>
+    </section>
+    <!-- END NEWS -->
+
+    <!-- START GAllery -->
+    <section class="py-12 bg-black text-white">
+        <div class="container mx-auto px-4">
+            <div class="grid lg:grid-cols-3 gap-6">
+                <div class="col-span-3 lg:col-span-1">
+                    <h3 class="pb text-3xl font-semibold">Here is Our Gallery</h3>
+                    <hr style="border-bottom: 4px solid #db4436;" class="text-secondary border-solid w-16 inline-block border-t-0 ">
+                    <p class="mt-2">Perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam doloremque laudantium, totam rem aperiam.</p>
+                </div>
+                <div class="col-span-3 lg:col-span-2">
+                    <div class="grid grid-cols-4 gap-2">
+                        <?php for ($i = 0; $i < 8; $i++) : ?>
+                            <img style="width:100%" loading="lazy" src="https://picsum.photos/375/200.webp?random=<?php echo $i ?>.webp" alt="">
+                        <?php endfor; ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- END GAllery -->
     <?php wp_footer(); ?>
 
 </body>
